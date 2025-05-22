626b48f3f523ae7d56ab8ba240468a5417bb0527
Akhmad Fakhoni Listiyan Dede <akhfa@users.noreply.github.com>
2025-05-22 21:28:21 +0700

    feat: eks clusters (#1)
    * feat: init tf code
    
    * feat: terraform tf workflow
    
    * fix: get aws access key from vars
    
    * fix: fixed tf fmt
    
    * chore: remove continue-on-error
    
    * chore: manage comments, change ami type to AL2023_x86_64_STANDARD, and remove ebs csi driver
    
    * fix: add write PR permission
    
    * chore: remove branch causing duplicate trigger

---
+++ terraform.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+  }
+
+  required_version = "~> 1.12.0"
+
+  backend "s3" {
+    bucket = "test-mwaa-akhfa"
+    key    = "terraform-state/testing.tfstate"
+    region = "ap-southeast-3"
+  }
+}
